8306c72ca6aff8dfaee229d5393c48e9295d61c7
Ruben <rsilva4@ucol.mx>
2024-05-28 10:05:19 -0600

    cambios en stream

--- stream.py
+++ stream.py
@@ -6,24 +6,25 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import plotly.graph_objects as go
 import geopandas as gpd # Para manejo de datos geoespaciales 
-
-
+import plotly.express as px
 
 reportes_df = pd.read_csv('reportes_agua_hist.csv')
 colonias_gdf = gpd.read_file('colonias_conteo.geojson')
+
+
 
 # Filtrar reportes para el año 2021
 reportes_df['fecha'] = pd.to_datetime(reportes_df['fecha'])
 reportes_2021 = reportes_df[reportes_df['fecha'].dt.year == 2021]
 #eliminar los valores NaN
 reportes_2021 = reportes_2021.dropna()
-planteamiento = """
-La falta de agua el la capital del país, no es una noticia nueva, con la llegada de esta temprada de calor, se ha vuelto un problema más recurrente, todos los días recibimos noticias sobre la ciudad de México que los vecinos pelean a capa y espada las pocas pipas de agua que llegan a suministrar este preciado líquido que es vital para todos. Sin embargo este problema no es único de esta temporada, es un problema concurrente y durante esta investigación hemos analizado la información de la SACMEX para poder entender un poco más sobre este problema. 
-"""
+
 
 # Título y descripción de la aplicación
 st.title('Reportes de falta de agua en CDMX')
-st.markdown(planteamiento)
+st.markdown("""
+En esta investigacion se analizan los reportes de falta de agua en la Ciudad de México durante el año 2021. Los datos se obtuvieron de la plataforma de Datos Abiertos de la Ciudad de México y se utilizaron técnicas de visualización de datos para explorar y analizar los reportes de falta de agua en la Ciudad de México. A continuación, se presentan los hallazgos y visualizaciones más relevantes de la investigación.
+""")
 
 # Mostrar el mapa
 st.markdown('## Mapa de reportes de falta de agua en CDMX')
@@ -37,6 +38,7 @@ de los reportes.
 with open('mapa.html', 'r', encoding='utf-8') as f:
     html_data = f.read()
 components.html(html_data, width=700, height=500)
+st.caption("Mapa 1: mapa interactivo de la ciudad de Mexico, creado por Hernandez Paredes Roberto Alejandro")
 
 # Función para reproducir el audio usando JavaScript
 def play_audio(audio_file):
@@ -90,7 +92,7 @@ fig.add_trace(go.Bar(
     x=reportes_por_alcaldia['Número de reportes'],
     y=reportes_por_alcaldia['Alcaldía'],
     orientation='h',
-    marker=dict(color='blue', line=dict(color='rgba(58, 71, 80, 1.0)', width=1)),
+    marker=dict(color='#19468D', line=dict(color='black', width=1)),
     name='Número de reportes'
 ))
 
@@ -103,6 +105,7 @@ fig.update_layout(
 
 # Mostrar la gráfica en Streamlit
 st.plotly_chart(fig, width=1000, height=500)
+st.caption("Grafica 1: Cantidad de reportes  de falta de agua por alcaldia a lo largo del tiempo en 2021.")
 
 # Mostrar la gráfica de barras de reportes por colonia
 st.markdown('## Reportes de falta de agua por colonia en Coyoacán en 2021')
@@ -126,7 +129,7 @@ fig_colonias.add_trace(go.Bar(
     x=colonias_filtradas['conteo'],
     y=colonias_filtradas['colonia'],
     orientation='h',
-    marker=dict(color='Blue', line=dict(color='Black', width=1)),
+    marker=dict(color='#19468D', line=dict(color='Black', width=1)),
     name='Número de reportes'
 ))
 
@@ -139,6 +142,8 @@ fig_colonias.update_layout(
 
 # Mostrar la gráfica en Streamlit
 st.plotly_chart(fig_colonias)
+st.caption(f"Grafica 2: Reportes de fallas en el suministro de agua por colonia en {alcaldia_seleccionada} con más de 100 reportes en 2021.")
+
 
 # Título y descripción
 st.markdown('## Reportes de falta de agua por colonia en Coyoacán en 2021')
@@ -152,3 +157,120 @@ df = pd.read_csv('reportes_colonia_fechas.csv')
 
 # Mostrar la tabla
 st.dataframe(df, width=1000, height=500)
+
+####Analisis de temporada ####
+
+# Mostrar la línea de tiempo de reportes
+st.markdown('## Línea de tiempo de reportes de falta de agua en CDMX en 2021')
+st.markdown("""
+Esta línea de tiempo muestra la evolución de los reportes de falta de agua en la Ciudad de México a lo largo del año 2021.
+Puedes interactuar con la gráfica para ver los detalles de los reportes en cada mes.
+""")
+
+# Supongo que tienes los datos en un DataFrame llamado reportes_2021
+# Si no lo tienes, carga los datos adecuados aquí
+reportes_2021['fecha'] = pd.to_datetime(reportes_2021['fecha'])
+reportes_2021['mes'] = reportes_2021['fecha'].dt.to_period('M')
+
+reportes_por_mes = reportes_2021.groupby('mes').size().reset_index(name='Número de reportes')
+reportes_por_mes['mes'] = reportes_por_mes['mes'].dt.to_timestamp()  # Convertir a timestamp
+
+# Crear la línea de tiempo
+fig_timeline = px.line(reportes_por_mes, x='mes', y='Número de reportes', title='Evolución de reportes de falta de agua en CDMX en 2021')
+
+fig_timeline.update_layout(
+    xaxis_title='Mes',
+    yaxis_title='Número de reportes',
+    hovermode='x unified'
+)
+
+# Mostrar la línea de tiempo en Streamlit
+st.plotly_chart(fig_timeline)
+st.caption("Grafica 3: Evolución de los reportes de falta de agua en la Ciudad de México a lo largo del año 2021.")
+
+
+
+
+# Análisis de Día de la Semana
+st.markdown('## Análisis por Día de la Semana')
+st.markdown("""
+Esta sección muestra si hay días de la semana con más reportes de falta de agua.
+""")
+
+# Agregar columna de día de la semana
+reportes_2021['dia_semana'] = reportes_2021['fecha'].dt.day_name()
+
+# Grafica la cantidad de reportes por día de la semana
+reportes_por_dia = reportes_2021['dia_semana'].value_counts().reindex([
+    'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'])
+fig_dia = go.Figure()
+
+fig_dia.add_trace(go.Bar(
+    x=reportes_por_dia.index,
+    y=reportes_por_dia.values,
+    marker=dict(color='#19468D', line=dict(color='rgba(58, 71, 80, 1.0)', width=1)),
+    name='Número de reportes'
+))
+
+fig_dia.update_layout(
+    title='Cantidad de reportes por día de la semana en 2021',
+    xaxis_title='Día de la semana',
+    yaxis_title='Número de reportes',
+    clickmode='event+select'
+)
+st.plotly_chart(fig_dia)
+st.caption("Grafica 4: Cantidad de reportes de falta de agua por día de la semana en 2021.")
+
+
+
+
+##### Graficas de ruben #####
+
+#abrir el archivo
+
+st.markdown('## Caracteristicas de las viviendas en colonias con mas reportes de falta de agua')
+
+
+# Lectura del archivo CSV
+reportes_df = pd.read_csv('sumado.csv')
+
+# Renombrar las columnas del DataFrame
+reportes_df.rename(columns={
+    "VPH_AEASP": "Disponen de agua entubada",
+    "VPH_AGUAFV": "No disponen de agua entubada",
+    "VPH_TINACO": "Tienen tinaco",
+    "VPH_CISTER": "Tienen cisterna",
+    "VPH_EXCSA": "Tienen excusado",
+    "VPH_LETR": "Tienen letrina",
+    "VPH_DRENAJ": "Tienen drenaje",
+    "VPH_NODREN": "No tienen drenaje",
+    "VPH_CEL": "Tienen teléfonos",
+    "VPH_TV": "Tienen televisión"
+}, inplace=True)
+
+# Crear la figura
+df_sums = reportes_df.drop(columns=['count'])
+df_sums = df_sums.sum().reset_index()
+df_sums.columns = ['Característica', 'Cantidad']
+
+fig_viviendas = go.Figure()
+
+fig_viviendas.add_trace(go.Bar(
+    x=df_sums['Cantidad'],
+    y=df_sums['Característica'],
+    orientation='h',
+    marker=dict(color='#19468D', line=dict(color='Black', width=1)),
+    name='Cantidad de viviendas'
+))
+
+fig_viviendas.update_layout(
+    #title='Características de las viviendas en Coyoacán',
+    xaxis_title='Cantidad de viviendas',
+    yaxis_title='Características de las viviendas',
+    clickmode='event+select'
+)
+
+# Mostrar la gráfica en Streamlit
+st.plotly_chart(fig_viviendas)
+st.caption("Grafica 5: Características de las viviendas en la alcaldía de Coyoacán.")
+
